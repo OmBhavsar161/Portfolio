ed4a08a842fc5938143b4cba71fcb7168e7b2369
Supernova161 <ombhavsar2004@gmail.com>
2024-07-12 22:59:24 +0530

    button

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,21 +1,39 @@
 export default function Navbar() {
-  const hover = "hover:bg-teal-300 rounded-md px-3 py-2"
+  const hover = "hover:text-teal-900 hover:bg-teal-300 rounded-md px-4 py-2 ";
   return (
-   <>
-     <nav className="flex space-x-80 bg-teal-700 py-3">
-        <h2 className="pl-52 text-3xl text-teal-100 ">Om Bhavsar</h2>
-        <ul className="flex font-bold text-white space-x-2 float-right pl-60 ">
-            <li className={hover}><button>Home</button></li>
-            <li className={hover}><button>Skill</button></li>
-            <li className={hover}><button>About</button></li>
-            <li className={hover}><button>Education</button></li>
-            <li className={hover}><button>Service</button></li>
-            <li className={hover}><button>Blog</button></li>
-            <li className={hover}><button>Contact</button></li>
+    <>
+      <nav className="relative z-10 flex space-x-80 bg-teal-700 py-3">
+        <h2 className="pl-52 text-3xl text-teal-100">Om Bhavsar</h2>
+        <ul className="flex font-bold text-white space-x-2 pl-48">
+          <li>
+            <button className={hover}>Home</button>
+          </li>
+          <li>
+            <button className={hover}>Skill</button>
+          </li>
+          <li>
+            <button className={hover}>About</button>
+          </li>
+          <li>
+            <button className={hover}>Education</button>
+          </li>
+          <li>
+            <button className={hover}>Service</button>
+          </li>
+          <li>
+            <button className={hover}>Blog</button>
+          </li>
+          <li>
+            <button className={hover}>Contact</button>
+          </li>
         </ul>
-     </nav>
-     <img src="./image5.jpg" className="w-full h-[36rem] object-cover"/>
-     <div className="absolute inset-0 flex items-center text-teal-100 justify-center text-6xl ">Om Bhavsar</div>
-     </>
-  )
+      </nav>
+      <div className="relative w-full h-[36rem]">
+        <img src="./image5.jpg" className="w-full h-[36rem] object-cover" />
+        <div className="absolute inset-0 flex items-center justify-center text-teal-100 text-6xl z-0">
+          Om Bhavsar
+        </div>
+      </div>
+    </>
+  );
 }
